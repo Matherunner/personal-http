e6a4a4eabc193a52206289ae8c58d1eddc1a86f2
Matherunner <randolphchong@gmail.com>
2015-01-25 17:13:29 +0800

    Implemented basic file upload

---
+++ httpserve.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"html/template"
+	"io"
+	"log"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+func logConnection(r *http.Request) {
+	log.Printf("[%s] %s %s\n", r.RemoteAddr, r.Method, r.URL)
+}
+
+func serveRoot(w http.ResponseWriter, r *http.Request) {
+	logConnection(r)
+	w.WriteHeader(http.StatusNotFound)
+}
+
+func serveList(w http.ResponseWriter, r *http.Request) {
+	logConnection(r)
+}
+
+func serveUpload(w http.ResponseWriter, r *http.Request) {
+	logConnection(r)
+	if r.Method == "GET" {
+		tmpl, err := template.ParseFiles("upload.gtpl")
+		if err != nil {
+			log.Fatalf("Failed to parse upload.gtpl (%s)\n", err.Error())
+		}
+
+		err = tmpl.Execute(w, nil)
+		if err != nil {
+			log.Printf("Failed to write upload form to clien (%s)\n:", err.Error())
+		}
+	} else if r.Method == "POST" {
+		w.WriteHeader(http.StatusNoContent)
+		mpReader, err := r.MultipartReader()
+		if err != nil {
+			log.Printf("[%s] Failed to read multipart form (%s)\n",
+				r.RemoteAddr, err.Error())
+			return
+		}
+
+		log.Printf("[%s] Got multipart form of size %d\n",
+			r.RemoteAddr, r.ContentLength)
+
+		var part *multipart.Part
+		for {
+			part, err = mpReader.NextPart()
+			if err == io.EOF {
+				return
+			} else if err != nil {
+				continue
+			}
+
+			if part.FileName() != "" {
+				log.Printf("[%s] Downloading \"%s\"\n", r.RemoteAddr, part.FileName())
+				break
+			}
+
+			part.Close()
+		}
+		defer part.Close()
+
+		outfile, err := os.OpenFile(filepath.Base(part.FileName()),
+			os.O_WRONLY | os.O_CREATE | os.O_EXCL, 0600)
+		if err != nil {
+			log.Printf("[%s] Failed to create file (%s)\n", r.RemoteAddr, err.Error())
+			return
+		}
+		defer outfile.Close()
+
+		_, err = io.Copy(outfile, part)
+		if err != nil {
+			log.Printf("[%s] Error occured when transferring \"%s\" (%s)\n",
+				r.RemoteAddr, part.FileName(), err.Error())
+			return
+		}
+
+		log.Printf("[%s] Transfer of \"%s\" completed\n", r.RemoteAddr, part.FileName())
+	}
+}
+
+func main() {
+	http.HandleFunc("/", serveRoot)
+	http.HandleFunc("/list", serveList)
+	http.HandleFunc("/upload", serveUpload)
+
+	log.Println("Server started.")
+	err := http.ListenAndServe(":9090", nil)
+	if err != nil {
+		log.Fatalf("Failed to listen to port (%s)\n", err.Error())
+	}
+}
---
+++ upload.gtpl
@@ -0,0 +1,62 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Send to file</title>
+<style>
+.mainbtn {
+  display: block;
+  font-size: 12pt;
+  margin: 10px auto 0 auto;
+  outline: none;
+  padding: 10px;
+  width: 100%;
+  background-color: #dedede;
+  border: 0;
+}
+
+.mainbtn::-moz-focus-inner {
+  border: 0;
+}
+
+.mainbtn:hover {
+  background-color: #ccc;
+}
+
+.mainbtn:active {
+  background-color: #afafaf;
+}
+
+.mainbtn:disabled {
+  background-color: #f3f3f3;
+  color: #aaa;
+}
+
+#maindiv {
+  position: relative;
+  margin: 0 auto 0 auto;
+  max-width: 1000px;
+  font-family: sans-serif;
+}
+</style>
+<script>
+function selfile(f) {
+  f.file.click();
+}
+
+function handlefile(f) {
+  f.select.value = f.file.files[0].name;
+  f.upload.disabled = false;
+}
+</script>
+</head>
+<body>
+<div id="maindiv">
+<form action="/upload" enctype="multipart/form-data" method="post">
+<input type="file" name="file" onchange="handlefile(this.form);" style="display:none">
+<input class="mainbtn" type="button" name="select" value="Select a file" onclick="selfile(this.form);">
+<input class="mainbtn" type="submit" name="upload" value="Send" disabled="disabled">
+</form>
+</div>
+</body>
+</html>
